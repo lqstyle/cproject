3f73f53672aa4f5f0c3fa840f9087183584c031d
lqstyle <lqstyle123@163.com>
2024-06-15 09:12:23 +0800

    新特性1

--- newFeature/OriginalLiteral.cpp
+++ newFeature/OriginalLiteral.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
-//
-//int main() {
-//
-//    std::string s = "d:\\hello\\world\\tell";
-//    std::string s1 = "d:\hello\world\tell";
-//    std::string s2 = R"("d:\hello\world\tell")";
-//    std::string s3 = R"111("d:\hello\world\tell")111";
-//    std::cout << s << std::endl;
-//    std::cout << s1 << std::endl;
-//    std::cout << s2 << std::endl;
-//    std::cout << s3 << std::endl;
-//    return 0;
-//}
+
+int main() {
+
+    std::string s = "d:\\hello\\world\\tell";
+    std::string s1 = "d:\hello\world\tell";
+    std::string s2 = R"("d:\hello\world\tell")";
+    std::string s3 = R"111("d:\hello\world\tell")111";
+    std::cout << s << std::endl;
+    std::cout << s1 << std::endl;
+    std::cout << s2 << std::endl;
+    std::cout << s3 << std::endl;
+    return 0;
+}
